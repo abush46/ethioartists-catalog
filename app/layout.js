3e31960e728618aaca97a827517212425e48954f
Michael Samson <40731418+abush46@users.noreply.github.com>
2025-01-02 19:36:06 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+<div><h1>Header</h1> 
+</div>
         {children}
       </body>
     </html>
